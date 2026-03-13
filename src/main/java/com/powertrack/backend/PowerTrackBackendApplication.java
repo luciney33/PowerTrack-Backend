5e73331aec72f3e92e4a34c7f50aaa49a35cf7c5
luciapenafiel <lpenafiel@cofrai.com>
2026-03-13 13:10:43 +0100

    Creación proyecto backend

---
+++ src/main/java/com/powertrack/backend/PowerTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.powertrack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerTrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerTrackBackendApplication.class, args);
+    }
+
+}
